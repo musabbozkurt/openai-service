c48cb538c21c5e974178160f130fee1fef369570
musabbozkurt <b_musab@hotmail.com>
2023-06-27 15:14:17 +0300

    Initial commit

---
+++ src/main/java/com/mb/openaiservice/config/OpenAIErrorDecoder.java
@@ -0,0 +1,30 @@
+package com.mb.openaiservice.config;
+
+import feign.Response;
+import feign.RetryableException;
+import feign.codec.ErrorDecoder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+
+import java.util.Date;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OpenAIErrorDecoder implements ErrorDecoder {
+
+    private final ErrorDecoder errorDecoder = new Default();
+
+    @Override
+    public Exception decode(String methodKey, Response response) {
+
+        Exception exception = errorDecoder.decode(methodKey, response);
+
+        if (HttpStatus.valueOf(response.status()) == HttpStatus.UNAUTHORIZED) {
+            log.warn("UNAUTHORIZED OpenAI request.");
+            throw new RetryableException(response.status(), exception.getMessage(), response.request().httpMethod(), exception, new Date(System.currentTimeMillis()), response.request());
+        }
+
+        return exception;
+    }
+}
